31f4a541bf1aaa8bca6b86fd1a2892e427c18d02
BitanXD <bitansarkar088@gmail.com>
2024-10-02 10:17:11 +0530

    admin setup added food items

--- fdw-frontend/src/components/LoginPopup/LoginPopup.jsx
+++ fdw-frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -4,6 +4,16 @@ import { assets } from '../../assets/assets'
 
 const LoginPopup = ({setShowLogin}) => {
     const [currentState, setCurrentState] = useState("Login")
+    const [data, setData] = useState({
+        name: "",
+        email: "",
+        password: ""
+    })
+    const onChangeHandler = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(data => ({...data, [name]: value}))
+    }
   return (
     <div className='login-popup'>
         <form className='login-popup-container' action="">
@@ -13,11 +23,11 @@ const LoginPopup = ({setShowLogin}) => {
             </div>
             <div className="login-popup-inputs">
                 {
-                    currentState === "Login" ? <></> : <input type="text" placeholder='Your name' required />
+                    currentState === "Login" ? <></> : <input name='name' onChange={onChangeHandler} value={data.name} type="text" placeholder='Your name' required />
                 }
                 
-                <input type="text" placeholder='Your email' required />
-                <input type="password" placeholder='Password' required />
+                <input name='email' onChange={onChangeHandler} value={data.email} type="text" placeholder='Your email' required />
+                <input name='password' onChange={onChangeHandler} value={data.password} type="password" placeholder='Password' required />
             </div>
             <button>{currentState === "Sign Up" ? "Create Account" : "Login"}</button>
             <div className="login-popup-condition">
