6a5c1563770365fd3c0f8af08fd0d3503c9fe242
BitanXD <bitansarkar088@gmail.com>
2024-12-19 17:40:13 +0530

    order model started payment stripe added

--- fdw-backend/server.js
+++ fdw-backend/server.js
@@ -1,33 +1,36 @@
-import express from 'express';
-import cors from 'cors';
-import { connectDB } from './config/db.config.js';
-import foodRouter from './routes/foodRoute.js';
-import userRouter from './routes/userRoute.js';
-import 'dotenv/config'
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.config.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 // app config
 const app = express();
 const port = 4000;
 
 // middleware
-app.use(express.json()) // request from frontend to backend will be parsed using this json
-app.use(cors()) // access the backend from the frontend
+app.use(express.json()); // request from frontend to backend will be parsed using this json
+app.use(cors()); // access the backend from the frontend
 
 // db connection
 connectDB();
 
 // api endpoints
-app.use("/api/food", foodRouter)
-app.use("/images", express.static('uploads'))
-app.use('/api/user', userRouter)
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
-    res.send("API WORKING")
-})
+  res.send("API WORKING");
+});
 
 app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`)
-})
-
+  console.log(`Server started on http://localhost:${port}`);
+});
 
 // mongodb+srv://bitansarkar088:<db_password>@cluster0.ev3cf.mongodb.net/?
